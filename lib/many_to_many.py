82cbec521e6c96d0a69bc628c124f346349308dc
Ben Goddard <goddard.ben04@gmail.com>
2025-12-03 13:02:03 -0700

    Completed many to many models

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -4,6 +4,11 @@ class Student:
         self.name = name
         self.email = email
         Student.all.append(self)
+    def courses(self):
+        return [schedule.course for schedule in Schedule.all if schedule.student == self]
+    def calculate_gpa(self):
+        grades = [schedule.grade for schedule in Schedule.all if schedule.student == self]
+        return sum(grades)/len(grades)
 
 class Course:
     all =[]
@@ -11,3 +16,30 @@ class Course:
         self.title = title
         self.teacher = teacher
         Course.all.append(self)
+    def students(self):
+        return [schedule.student for schedule in Schedule.all if schedule.course == self]
+
+class Schedule:
+    all = []
+    def __init__(self, student, course, grade):
+        self.student = student
+        self.course = course
+        self.grade = grade
+        Schedule.all.append(self)
+    @property
+    def student(self):
+        return self._student
+    @student.setter
+    def student(self, value):
+        if not isinstance(value, Student):
+            raise Exception
+        self._student = value
+
+    @property
+    def course(self):
+        return self._course
+    @course.setter
+    def course(self, value):
+        if not isinstance(value, Course):
+            raise Exception
+        self._course = value
